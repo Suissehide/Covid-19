d451afba42e36a61d98d0e55b44595cfbde095c0
Léo Couffinhal <leo.couffinhal@epitech.eu>
2020-04-20 17:53:57 +0200

    init

---
+++ web/src/views/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={"w-layout-grid grid"}>
+        <div id="w1">
+          <text> wow1 </text>
+        </div>
+        <div id="w2">
+          <text> wow2 </text>
+        </div>
+        <div id="w3">
+          <text> wow3 </text>
+        </div>
+        <div id="w4">
+          <text> wow4 </text>
+        </div>
+        <div id="w5">
+          <text> wow5 </text>
+        </div>
+        <div id="w6">
+          <text> wow6 </text>
+        </div>
+        <div id="w7">
+          <text> wow7 </text>
+        </div>
+        <div id="w8">
+          <text> wow8 </text>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
