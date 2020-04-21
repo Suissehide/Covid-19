7c9e32aba790e1727444e455219c0a874642ff0a
tolau <to.laulan@hotmail.fr>
2020-04-21 19:06:12 +0200

    init widget total death + css

---
+++ web/src/views/TotalDeath.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css/style.css'
+
+class TotalDeath extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div>Total death</div>
+                <div>Italy</div>
+            </div>
+        )
+    }
+}
