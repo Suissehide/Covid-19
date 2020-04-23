4a197bb6aa44e69bd904d9322bc8662e06260088
tolau <to.laulan@hotmail.fr>
2020-04-23 16:51:06 +0200

    Adding base for widget total death

--- web/src/views/TotalDeath.js
+++ web/src/views/TotalDeath.js
@@ -1,14 +1,31 @@
 import React from "react";
-import '../css/style.css'
+import '../css/style.css';
+import '../css/TotalDeath.css';
 
 class TotalDeath extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>Total death</div>
-                <div>Italy</div>
+            <div className="WidgetDeath">
+                <div className="TotalDeath">
+                    <div className="LetterDeath">Total Deaths</div>
+                    <div className="NumberDeath">184372</div>
+                </div>
+                <div className="Country">
+                    <div className="NameCountry">Italy</div>
+                    <div className="NumberCountry">25 085</div>
+                </div>
+                <div className="Country">
+                    <div className="NameCountry">Spain</div>
+                    <div className="NumberCountry">22 157</div>
+                </div>
+                <div className="Country">
+                    <div className="NameCountry">France</div>
+                    <div className="NumberCountry">21 340</div>
+                </div>
             </div>
         )
     }
 }
+
+export default TotalDeath;
