f7ad9de6a8bb9ee9325c3d88a8aa5acaa6df0e74
theo.goncalves <theo.goncalves@epitech.eu>
2020-04-23 23:07:37 +0200

    parser finished (and tested)

--- web/src/services/Parser.js
+++ web/src/services/Parser.js
@@ -4,9 +4,9 @@ import CSVReader from "react-csv-reader";
 
 const handleForce = (data, fileInfo) => {
     localStorage.setItem('csvData', JSON.stringify(data));
-    let issou = localStorage.getItem('csvData') === null ? [] : JSON.parse(localStorage.getItem('csvData'));
-    console.log(issou);
-    console.log(data)};
+//    let dataFromCsv = localStorage.getItem('csvData') === null ? [] : JSON.parse(localStorage.getItem('csvData'));
+//    console.log([[[data ou dataFromCsv]]])
+};
 
 const papaparseOptions = {
     header: true,
@@ -15,18 +15,18 @@ const papaparseOptions = {
     transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
 };
 
-const reader = (
-    <div className="container">
-        <CSVReader
-            cssClass="react-csv-input"
-            label="Select CSV with secret Death Star statistics"
-            onFileLoaded={handleForce}
-            parserOptions={papaparseOptions}
-        />
-        <p>and then open the console</p>
-    </div>
-);
+function Reader () {
+    return (
+        <div className="container">
+            <CSVReader
+                cssClass="react-csv-input"
+                label="Select CSV with secret Death Star statistics"
+                onFileLoaded={handleForce}
+                parserOptions={papaparseOptions}
+            />
+            <p>and then open the console</p>
+        </div>
+    );
+}
 
-export default reader;
-
-ReactDOM.render(reader, document.getElementById("root"));
+export default Reader;
