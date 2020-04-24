d42c8f1b3b870ba0bec6f3dab9a18529d5c864d2
tolau <to.laulan@hotmail.fr>
2020-04-24 15:16:15 +0200

    Final design

--- web/src/views/TotalDeath.js
+++ web/src/views/TotalDeath.js
@@ -9,19 +9,22 @@ class TotalDeath extends React.Component {
             <div className="WidgetDeath">
                 <div className="TotalDeath">
                     <div className="LetterDeath">Total Deaths</div>
-                    <div className="NumberDeath">184372</div>
+                    <div className="NumberDeath">191 962</div>
                 </div>
+                <div className="Separator"/>
                 <div className="Country">
                     <div className="NameCountry">Italy</div>
-                    <div className="NumberCountry">25 085</div>
+                    <div className="NumberCountry">25 549</div>
                 </div>
+                <div className="SeparatorCountry"/>
                 <div className="Country">
                     <div className="NameCountry">Spain</div>
-                    <div className="NumberCountry">22 157</div>
+                    <div className="NumberCountry">22 524</div>
                 </div>
+                <div className="SeparatorCountry"/>
                 <div className="Country">
                     <div className="NameCountry">France</div>
-                    <div className="NumberCountry">21 340</div>
+                    <div className="NumberCountry">21 856</div>
                 </div>
             </div>
         )
