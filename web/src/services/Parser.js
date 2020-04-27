e8e0da9f9cffcc25f82e9c8b3341194552d7db84
Theo <theo.goncalves@epitech.eu>
2020-04-27 15:27:19 +0200

    perfect parser with loading screen

--- web/src/services/Parser.js
+++ web/src/services/Parser.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import loadGif from "../assets/images/gif_load.gif";
+import "../css/Parser.css"
 
 class ApiCovid extends React.Component {
 
@@ -12,14 +14,16 @@ class ApiCovid extends React.Component {
         const api_call = await fetch('https://api.covid19api.com/summary');
         const data = await api_call.json();
         localStorage.setItem('covidData', JSON.stringify(data));
-        let dataFromJson = localStorage.getItem('covidData') === null ? [] : JSON.parse(localStorage.getItem('covidData'));
-        console.log(dataFromJson);
+//        let dataFromJson = localStorage.getItem('covidData') === null ? [] : JSON.parse(localStorage.getItem('covidData'));
+        this.props.handleLoading(true);
+//        console.log(dataFromJson);
     }
+
 
     render () {
         return (
-            <div>
-
+            <div className="gif">
+                <img src={loadGif}/>
             </div>
         );
     }
